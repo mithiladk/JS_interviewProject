b576a213d3ffcab42b1088d2bbcce4cc20595dac
mithila.dk <mithila.deekay@gmail.com>
2024-04-16 23:04:40 +0530

    todoapp in vanilla js

---
+++ todo-app/script.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const todoForm = document.querySelector(".todo-form");
+    const todoInput = document.querySelector(".todo-input");
+    const todoList = document.querySelector(".todo-List");
+    const todoSubmit = document.querySelector(".todo-submit");
+    let editMode = false;
+    let editItem = null;
+
+todoForm.addEventListener("submit",function(event){
+    event.preventDefault();
+    const todoText = todoInput.value.trim();
+    if(todoText !== ""){
+
+        if(editMode){
+            editItem.firstChild.textContent = todoText;
+            todoSubmit.innerText = "Add Todo";
+            editMode = false;
+            editItem = null
+        }
+        else {
+            addTodoItem(todoText)  
+        }
+        //add todos
+        
+        todoInput.value = " ";
+    } else {
+        alert("Please enter a valid task")
+    }
+})
+
+todoList.addEventListener("click",function(event){
+    const target = event.target;
+    if(target.tagName === "BUTTON") {
+        const todoItem = target.parentNode;
+        if(target.innerText = `❌`){
+            todoItem.remove();
+        }else if(target.innerText === `🖊`){
+            editMode = true;
+            editItem = todoItem
+            todoSubmit.innerText = 'Edit Todo';
+            todoInput.value = todoItem.firstChild.textContent;
+            todoInput.focus();
+        }
+    }
+})
+
+function addTodoItem(todoText){
+    const todoItem = document.createElement("li");
+    const editButton = document.createElement("button");
+    const removeButton = document.createElement("button");
+
+    todoItem.innerHTML = `<span>${todoText}</span>`
+    editButton.innerText = `🖊`;
+    removeButton.innerText = `❌`
+
+    todoItem.appendChild(editButton)
+    todoItem.appendChild(removeButton)
+    todoList.appendChild(todoItem);
+}
+
+})
+
